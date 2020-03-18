a9d127854171edf241d0c5da8a4538125c3cbcbc
Ray <jialei@datastory.com.cn>
2020-03-18 19:42:39 +0800

    测送

---
+++ src/components/HelloAbout.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello-about">
+    <input v-model="msg" />
+    <p>prop: {{propMessge}}</p>
+    <p>msg: {{msg}}</p>
+    <p>helloMsg: {{helloMsg}}}</p>
+    <p>computed msg: {{computedMsg}}}</p>
+    <button @click="greet">greet</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {
+  @Prop() propMessge!: string;
+
+  //initial data
+  msg = 123;
+
+  // use prop values for initial data
+  helloMsg = "Hello, " + this.propMessge;
+
+  // lifecycle hook
+  mounted() {
+    this.greet();
+  }
+
+  // couputed
+  get computedMsg(): string {
+    return "computed " + this.msg;
+  }
+
+  //method
+  greet(): void {
+    console.log("greeting: ", this.msg);
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <hello-about propMessge="Messge" />
   </div>
 </template>
+
+<script>
+import HelloAbout from "@/components/HelloAbout.vue";
+export default {
+  name: "About",
+  components: {
+    HelloAbout
+  }
+};
+</script>
